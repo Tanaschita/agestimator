ced677f3240292f50b50d2a57e6f33ebd382400a
Natascha Fadeeva <natascha@appocados.com>
2025-06-19 12:58:54 +0200

    Initial commit.

---
+++ Agestimator/AgestimatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgestimatorApp.swift
+//  Agestimator
+//
+//  Created by Natascha Fadeeva on 19.06.25.
+//
+
+import SwiftUI
+
+@main
+struct AgestimatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
